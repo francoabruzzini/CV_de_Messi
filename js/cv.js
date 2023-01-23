452ce902a787e8dd78cd3050b86e51961ae2c606
Franco Abruzzini <franco.stone96@gmail.com>
2023-01-23 11:32:05 -0300

    Primer escritura en el repositorio

---
+++ js/cv.js
@@ -0,0 +1,16 @@
+//me costó mucho hacer funcionar estos botones y seguramente haya una manera más fácil, pero está prolijo
+ document.getElementById('boton_blanco').addEventListener('click', function() {
+ document.getElementById('titulo1').style.borderColor = 'white';   
+ document.getElementById('titulo2').style.borderColor = 'white';   
+ document.getElementById('titulo3').style.borderColor = 'white';
+ document.getElementById('titulo4').style.borderColor = 'white';
+ document.getElementById('titulo5').style.borderColor = 'white';   
+});
+
+document.getElementById('boton_celeste').addEventListener('click', function() {
+    document.getElementById('titulo1').style.borderColor = '#75AADB';   
+    document.getElementById('titulo2').style.borderColor = '#75AADB';   
+    document.getElementById('titulo3').style.borderColor = '#75AADB';
+    document.getElementById('titulo4').style.borderColor = '#75AADB';
+    document.getElementById('titulo5').style.borderColor = '#75AADB';   
+   });
